99154c35563f127885929602c01dd9beda6cc2a8
Ahmed Hossam <ahmedhossam114231@gmail.com>
2021-11-06 18:17:25 +0200

    tset

--- app/routes/resources/level.route.js
+++ app/routes/resources/level.route.js
@@ -4,22 +4,22 @@ const ctrls = require('../../controllers');
 
 let router = express.Router();
 
-router.get('/levels/:id', ctrls.LevelCtrl.fetchOne);
-router.get('/levels', ctrls.LevelCtrl.fetchAll);
-router.post(
-  '/levels',
-  policies.isAllowed(['admin']),
-  ctrls.LevelCtrl.CreateOne,
-);
-router.patch(
-  '/levels/:id',
-  policies.isAllowed(['admin']),
-  ctrls.LevelCtrl.UpdateOne,
-);
-router.delete(
-  '/levels/:id',
-  policies.isAllowed(['admin']),
-  ctrls.LevelCtrl.DeleteOne,
-);
+// router.get('/levels/:id', ctrls.LevelCtrl.fetchOne);
+// router.get('/levels', ctrls.LevelCtrl.fetchAll);
+// router.post(
+//   '/levels',
+//   policies.isAllowed(['admin']),
+//   ctrls.LevelCtrl.CreateOne,
+// );
+// router.patch(
+//   '/levels/:id',
+//   policies.isAllowed(['admin']),
+//   ctrls.LevelCtrl.UpdateOne,
+// );
+// router.delete(
+//   '/levels/:id',
+//   policies.isAllowed(['admin']),
+//   ctrls.LevelCtrl.DeleteOne,
+// );
 
 module.exports = router;
