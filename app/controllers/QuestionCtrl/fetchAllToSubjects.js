758d70c16b7f529696e5772aceb559366129b068
Ahmed Hossam <ahmedhossam114231@gmail.com>
2021-11-15 17:51:40 +0200

    a

--- app/controllers/QuestionCtrl/fetchAllToSubjects.js
+++ app/controllers/QuestionCtrl/fetchAllToSubjects.js
@@ -7,29 +7,39 @@ module.exports = $baseCtrl(async (req, res) => {
   if (isNaN(id)) return APIResponse.NotFound(res);
   const subject = await models.subject.findById(id);
   if (!subject) return APIResponse.NotFound(res, "NO Subject with that id");
-  if (req.me.subjects.indexOf(subject._id) === -1 && req.me.role !== "admin")
-    return APIResponse.Forbidden(res, "Dont allow to do this action");
 
-  const lessons = await models._question.fetchAll(
-    req.allowPagination,
-    {
-      ...req.queryFilter,
-      isChild: false,
-      subject: id,
-    },
-    {
-      ...req.queryOptions,
-      populate: [
-        { path: "subject", select: "nameAr nameEn" },
-        { path: "object", select: "nameAr nameEn" },
-        {
-          path: "childrenQuestions",
-          select:
-            "head modelAnswer choices numberOfInputs attachment parentQuestion image",
-        },
-      ],
+  if (req.me.role === "teacher" || req.me.role === "admin") {
+    console.log(req.me.role)
+    if (req.me.role === "teacher") {
+      if (req.me.subjects.indexOf(subject._id) === -1)
+        return APIResponse.Forbidden(res, "Dont allow to do this action");
     }
-  );
 
-  return APIResponse.Ok(res, lessons);
+
+    const lessons = await models._question.fetchAll(
+      req.allowPagination,
+      {
+        ...req.queryFilter,
+        isChild: false,
+        subject: id,
+      },
+      {
+        ...req.queryOptions,
+        populate: [
+          { path: "subject", select: "nameAr nameEn" },
+          { path: "object", select: "nameAr nameEn" },
+          {
+            path: "childrenQuestions",
+            select:
+              "head modelAnswer choices numberOfInputs attachment parentQuestion image",
+          },
+        ],
+      }
+    );
+
+    return APIResponse.Ok(res, lessons);
+  }
+  else {
+    return APIResponse.Forbidden(res, "Dont allow to do this action");
+  }
 });
